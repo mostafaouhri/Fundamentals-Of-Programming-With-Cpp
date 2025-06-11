0539f4a452a61a107612ddc59862dd8339654e3f
Mostafa Ouhri <ouhrimostafa@gmail.com>
2025-06-11 08:53:28 +0000

    update

--- README.cpp
+++ README.cpp
@@ -5,11 +5,23 @@ using namespace std;
 int main()
 {
       // Hello There who Are you ??
-      // Spying On My Code isn't ?  it Ok I'm Just Joking Hhh
+      // Spying On My Code isn't ?  it Ok I'm Just Joking Hhh .
       // I Lost the Code its bad !
-      // so that why I Thought at least I must write a list of what I Have studied Thanks .
+      // so that why I Thought at least I must write a list of what ...
+      // I Have studied Thanks .
 
-      cout << "";
-      cout << "";
+      string yes_no;
+      cout << "Do You Love C++ Or No ? \n";
+      cin >> yes_no;
+
+      if (yes_no == "yes" || yes_no == "Yes" || yes_no == "YES" || yes_no == "1")
+      {
+            cout << "intresting , your Are Like Me\n"
+                 << "It's low Level Langauge Fast And Speed \n";
+      }
+      else
+      {
+            cout << "The Problem with The Old Teaching Methods \n";
+      }
       return 0;
 }
